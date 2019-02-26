e0190105b10bd53922f299a361e2c3bb9be69a7b
kiwixz <kiwixz@outlook.com>
2019-02-26 23:08:02 +0000

    add unordered map to config

--- config/config.h
+++ config/config.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <unordered_map>
+
 namespace config {
 
 struct Config {
+private:
+    std::unordered_map<std::string, std::string> storage_;
 };
 
 }  // namespace config
