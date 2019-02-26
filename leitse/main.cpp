fa1879f15947d37c15150cf2b4cf505bc7015e4e
kiwixz <kiwixz@outlook.com>
2019-02-26 20:14:17 +0000

    cleanup main

--- leitse/main.cpp
+++ leitse/main.cpp
@@ -14,7 +14,7 @@ void main_impl(int argc, char** argv)
 int main(int argc, char** argv)
 {
     try {
-        std::string log_file_path = (std::filesystem::temp_directory_path() / "fractal.log").string();
+        std::string log_file_path = (std::filesystem::temp_directory_path() / "leitse.log").string();
         auto console_sink = std::make_shared<spdlog::sinks::stderr_color_sink_mt>();
         auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_file_path, true);
         auto logger = std::make_shared<spdlog::logger>("", spdlog::sinks_init_list{console_sink, file_sink});
@@ -23,16 +23,16 @@ int main(int argc, char** argv)
         spdlog::set_level(spdlog::level::debug);
 #endif
         spdlog::set_pattern("%^[%H:%M:%S.%f][%t][%l]%$ %v");
-        spdlog::debug("starting...");
+        spdlog::debug("started");
         main_impl(argc, argv);
-        spdlog::debug("terminating...");
+        spdlog::debug("terminated");
         spdlog::drop_all();
-        return 0;
     }
     catch (std::exception const& ex) {
         spdlog::critical("critical exception: {}", ex.what());
         return 1;
     }
+    return 0;
 }
 
 }  // namespace
