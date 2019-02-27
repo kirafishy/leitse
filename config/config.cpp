6db8448da12f63aa1e33f8fe90bd47b29ef391e2
kiwixz <kiwixz@outlook.com>
2019-02-27 17:14:51 +0000

    parse_global_config

--- config/config.cpp
+++ config/config.cpp
@@ -1,6 +1,5 @@
 #include "config/config.h"
 #include <utils/paths.h>
-#include <filesystem>
 
 namespace config {
 
@@ -44,7 +43,17 @@ int Config::parse_args(int argc, char const* const* argv, bool allow_unkown)
 }
 
 void Config::parse_global_config(std::string_view const& app_name)
-{}
+{
+    constexpr std::string_view file_name = "config.ini";
+
+    auto parse_if_exists = [&](std::filesystem::path const& path) {
+        if (std::filesystem::exists(path))
+            parse_file(path.string());
+    };
+
+    parse_if_exists(utils::get_kiwixz_home(app_name) / file_name);
+    parse_if_exists(file_name);
+}
 
 void Config::parse_file(std::string_view const& path, bool allow_unkown)
 {
