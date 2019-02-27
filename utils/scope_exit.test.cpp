e2ef43b0990141c97223a214e5518638562ad9e1
kiwixz <kiwixz@outlook.com>
2019-02-27 00:22:51 +0000

    update include paths from utils

--- utils/scope_exit.test.cpp
+++ utils/scope_exit.test.cpp
@@ -1,4 +1,4 @@
-#include "scope_exit.h"
+#include "utils/scope_exit.h"
 #include <doctest/doctest.h>
 
 namespace utils::test {
--- utils/thread_pool.test.cpp
+++ utils/thread_pool.test.cpp
@@ -1,4 +1,4 @@
-#include "thread_pool.h"
+#include "utils/thread_pool.h"
 #include <doctest/doctest.h>
 #include <memory>
 
