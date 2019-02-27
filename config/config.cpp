6962d83583b91b0a8efdb609ee6d372be3882a85
kiwixz <kiwixz@outlook.com>
2019-02-27 03:02:58 +0000

    config map proxy

--- config/config.cpp
+++ config/config.cpp
@@ -1,13 +1,46 @@
 #include "config/config.h"
+#include <stdexcept>
 
 namespace config {
 
+std::string& Config::operator[](std::string& const key)
+{
+    return map_[key];
+}
+
+std::string& Config::at(std::string& const key)
+{
+    return map_.at(key);
+}
+
+std::string const& Config::at(std::string& const key) const
+{
+    return map_.at(key);
+}
+
+bool Config::exists(std::string& const key)
+{
+    return map_.count(key) > 0;
+}
+
+void Config::remove(std::string& const key)
+{
+    if (map_.erase(key) == 0)
+        throw std::runtime_error{"key not present"};
+}
+
 int Config::parse(int argc, char const* const* argv, bool allow_unkown)
 {
+    (void)argc;
+    (void)argv;
+    (void)allow_unkown;
     return 0;
 }
 
 void Config::parse_file(std::string_view const& path, bool allow_unkown)
-{}
+{
+    (void)path;
+    (void)allow_unkown;
+}
 
 }  // namespace config
--- config/include/config/config.h
+++ config/include/config/config.h
@@ -5,14 +5,30 @@
 namespace config {
 
 struct Config {
+    std::string& operator[](std::string& const key);
+    std::string& at(std::string& const key);
+    std::string const& at(std::string& const key) const;
+    bool exists(std::string& const key);
+    void remove(std::string& const key);
+
     /// Parse command-line arguments and look for an config file.
     /// Return the number of parsed arguments, so you can handle positional arguments.
     int parse(int argc, char const* const* argv, bool allow_unkown = false);
 
     void parse_file(std::string_view const& path, bool allow_unkown = false);
 
+    template <typename T>
+    T get(std::string& const key) const;
+
 private:
     std::unordered_map<std::string, std::string> map_;
 };
+
+
+template <typename T>
+T Config::get(std::string& const key) const
+{
+    return T{key};
+}
 
 }  // namespace config
