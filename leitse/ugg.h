39a765c36f8f098401d36daa42316b73358dee6a
kiwixz <kiwixz@outlook.com>
2019-03-01 21:53:15 +0000

    remove redundant virtual

--- leitse/ugg.h
+++ leitse/ugg.h
@@ -5,8 +5,8 @@
 namespace leitse::aggregators {
 
 struct Ugg : Aggregator {
-    virtual std::vector<std::pair<std::string, std::string>> get_options() const override;
-    virtual std::vector<ItemSet> get_itemsets(Champion const& champion) const override;
+    std::vector<std::pair<std::string, std::string>> get_options() const override;
+    std::vector<ItemSet> get_itemsets(Champion const& champion) const override;
 };
 
 }  // namespace leitse::aggregators
