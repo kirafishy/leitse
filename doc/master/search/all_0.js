cfe5c8562acfc7d758f50035fb53d04925052ede
ci <ci>
2019-03-01 21:39:09 +0000

    [ci] update 'doc/master' commit 352ff636593020dee1d6aff6447f7913a4e9c032

--- doc/master/search/all_0.js
+++ doc/master/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['aggregator',['Aggregator',['../structleitse_1_1_aggregator.html',1,'leitse::Aggregator'],['../structleitse_1_1_aggregator.html#a30db9064f036eee5e988079f5e6b62c2',1,'leitse::Aggregator::Aggregator()']]],
+  ['aggregator',['Aggregator',['../structleitse_1_1_aggregator.html',1,'leitse::Aggregator'],['../structleitse_1_1_aggregator.html#ae6b9f221a890548bf8fca720fef958e0',1,'leitse::Aggregator::Aggregator()=default'],['../structleitse_1_1_aggregator.html#a76efdf616f0497d0fb887583acd29933',1,'leitse::Aggregator::Aggregator(Aggregator const &amp;)=delete'],['../structleitse_1_1_aggregator.html#a60e8c3e46b27293e5221d03a4e3eddd7',1,'leitse::Aggregator::Aggregator(Aggregator &amp;&amp;)=delete']]],
   ['aggregator_2ecpp',['aggregator.cpp',['../aggregator_8cpp.html',1,'']]],
   ['aggregator_2eh',['aggregator.h',['../aggregator_8h.html',1,'']]]
 ];
--- doc/master/search/functions_0.js
+++ doc/master/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['aggregator',['Aggregator',['../structleitse_1_1_aggregator.html#a30db9064f036eee5e988079f5e6b62c2',1,'leitse::Aggregator']]]
+  ['aggregator',['Aggregator',['../structleitse_1_1_aggregator.html#ae6b9f221a890548bf8fca720fef958e0',1,'leitse::Aggregator::Aggregator()=default'],['../structleitse_1_1_aggregator.html#a76efdf616f0497d0fb887583acd29933',1,'leitse::Aggregator::Aggregator(Aggregator const &amp;)=delete'],['../structleitse_1_1_aggregator.html#a60e8c3e46b27293e5221d03a4e3eddd7',1,'leitse::Aggregator::Aggregator(Aggregator &amp;&amp;)=delete']]]
 ];
