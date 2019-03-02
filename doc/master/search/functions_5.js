75026a126de580e329e9662b65bb83a78a354d1a
ci <ci>
2019-03-02 01:00:10 +0000

    [ci] update 'doc/master' commit d3713a96bfa45803e192e16e63ca725f246fbcf6

--- doc/master/search/functions_5.js
+++ doc/master/search/functions_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['fetch_5fdata',['fetch_data',['../structleitse_1_1_data_dragon.html#a5f18e0aeffbfad2a8b2df227bdf5d04b',1,'leitse::DataDragon']]],
-  ['fetch_5furl',['fetch_url',['../structleitse_1_1_data_dragon.html#a422568ae092dfecc957a04c2ece93c29',1,'leitse::DataDragon']]]
+  ['fetch_5fdata',['fetch_data',['../structleitse_1_1_data_dragon.html#a99b1d9414c57d987ee8aca3ec804f738',1,'leitse::DataDragon']]]
 ];
