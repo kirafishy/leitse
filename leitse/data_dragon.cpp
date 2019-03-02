a70fcbea89e3e31a6b923c5423ed17573054bb02
kiwixz <kiwixz@outlook.com>
2019-03-02 01:53:41 +0000

    data_dragon get version and champions

--- leitse/data_dragon.cpp
+++ leitse/data_dragon.cpp
@@ -1,7 +1,18 @@
 #include "data_dragon.h"
 #include <cpr/cpr.h>
+#include <spdlog/spdlog.h>
+
+#include <iostream>
 
 namespace leitse {
+
+Champion::Champion(nlohmann::json const& json)
+{
+    json.at("key").get_to(key);
+    json.at("id").get_to(key);
+    json.at("name").get_to(name);
+}
+
 
 std::vector<Champion> const& DataDragon::champions()
 {
@@ -10,14 +21,34 @@ std::vector<Champion> const& DataDragon::champions()
 
 void DataDragon::populate()
 {
-    fetch_data("champions.json");
+    update_version();
+
+    nlohmann::json champions_json = fetch_data("champion");
+    nlohmann::json const& champions_data_json = champions_json.at("data");
+    champions_.clear();
+    champions_.reserve(champions_data_json.size());
+    for (auto const& champion_json : champions_json.at("data"))
+        champions_.emplace_back(champion_json);
+    spdlog::info("[data_dragon] found {} champions", champions_.size());
 }
 
-nlohmann::json DataDragon::fetch_data(std::string_view const& data)
+void DataDragon::update_version()
 {
-    cpr::Response response = cpr::Get(cpr::Url{ "https://api.github.com/repos/whoshuu/cpr/contributors" });
-    printf("%s\n", response.text.c_str());
-    return {};
+    cpr::Response response = cpr::Get(cpr::Url{"https://ddragon.leagueoflegends.com/api/versions.json"});
+    if (response.status_code >= 400)
+        throw std::runtime_error{"could not update data dragon version"};
+    nlohmann::json json = nlohmann::json::parse(response.text);
+    version_ = json[0].get<std::string>();
+    spdlog::info("[data_dragon] using version {}", version_);
 }
 
+nlohmann::json DataDragon::fetch_data(std::string_view const& filename)
+{
+    cpr::Url url = fmt::format("http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/{}.json", version_, filename);
+    cpr::Response response = cpr::Get(url);
+    if (response.status_code >= 400)
+        throw std::runtime_error{fmt::format("could not update get '{}' from data dragon", filename)};
+    return nlohmann::json::parse(response.text);
 }
+
+}  // namespace leitse
--- leitse/data_dragon.h
+++ leitse/data_dragon.h
@@ -1,12 +1,20 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
+#include <string>
 #include <string_view>
 #include <vector>
 
 namespace leitse {
 
-struct Champion {};
+struct Champion {
+    std::string key;
+    std::string id;
+    std::string name;
+
+    Champion() = default;
+    Champion(nlohmann::json const& json);
+};
 
 
 struct DataDragon {
@@ -15,8 +23,10 @@ struct DataDragon {
     void populate();
 
 private:
+    std::string version_;
     std::vector<Champion> champions_;
 
+    void update_version();
     nlohmann::json fetch_data(std::string_view const& data);
 };
 
