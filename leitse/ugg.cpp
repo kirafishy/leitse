ff445f865993c77e7437c999ebe6934e19b6fb13
kiwixz <kiwixz@outlook.com>
2019-03-02 19:40:58 +0000

    add miscellaneous items

--- leitse/ugg.cpp
+++ leitse/ugg.cpp
@@ -78,27 +78,36 @@ std::vector<ItemSet> Ugg::itemsets(Champion const& champion) const
 
     nlohmann::json data = nlohmann::json::parse(response.text).at("12").at("10");  // 12=world 10=platinum+
     std::vector<ItemSet> item_sets;
-    std::set<int> added_items;
-    std::set<ItemCandidate, ItemCandidateComparator> item_candidates;
 
-    auto add_candidate = [&](ItemCandidate candidate) {
-        if (added_items.count(candidate.id) != 0)
-            return;
-        auto it = item_candidates.find(candidate.id);  // we are comparing only ids
-        if (it != item_candidates.end())
-            if (candidate < *it)
-                item_candidates.erase(it);
-            else
-                return;
-        item_candidates.insert(std::move(candidate));
-    };
+    ItemSet::Block miscellaneous_block;
+    miscellaneous_block.name = "Miscellaneous";
+    miscellaneous_block.items.push_back({3340});  // yellow trinket
+    miscellaneous_block.items.push_back({3364});  // red trinket
+    miscellaneous_block.items.push_back({3363});  // blue trinket
+    miscellaneous_block.items.push_back({2055});  // control ward
+    miscellaneous_block.items.push_back({2003});  // health potion
+    miscellaneous_block.items.push_back({2140});  // ad elixir
+    miscellaneous_block.items.push_back({2139});  // ap elixir
+    miscellaneous_block.items.push_back({2138});  // tank elixir
+    miscellaneous_block.items.push_back({2047});  // oracle lens
 
     for (Role const& role : roles) {
-        if (role.id != "3")  // temporary
-            continue;
-
         nlohmann::json const& data_role = data.at(role.id);
         ItemSet& item_set = item_sets.emplace_back(name(), ItemSet::Map::any, role.name);
+
+        std::set<int> added_items;
+        std::set<ItemCandidate, ItemCandidateComparator> item_candidates;
+        auto add_candidate = [&](ItemCandidate candidate) {
+            if (added_items.count(candidate.id) != 0)
+                return;
+            auto it = item_candidates.find(candidate.id);  // we are comparing only ids
+            if (it != item_candidates.end())
+                if (candidate < *it)
+                    item_candidates.erase(it);
+                else
+                    return;
+            item_candidates.insert(std::move(candidate));
+        };
 
         auto add_items_to_block = [&](ItemSet::Block& block,
                                       nlohmann::json const& data_block,
@@ -123,18 +132,18 @@ std::vector<ItemSet> Ugg::itemsets(Champion const& champion) const
                 }
             }
 
-            while (!item_candidates.empty()) {
-                ItemCandidate item = item_candidates.extract(item_candidates.begin()).value();
-                block.items.push_back({item.id, std::min(100 * item.matches / total_matches, 1)});
-                added_items.insert(item.id);
-            }
+            if (add_candidates)
+                while (!item_candidates.empty()) {
+                    ItemCandidate item = item_candidates.extract(item_candidates.begin()).value();
+                    block.items.push_back({item.id, std::max(100 * item.matches / total_matches, 1)});
+                    added_items.insert(item.id);
+                }
         };
 
         ItemSet::Block* block = &item_set.blocks.emplace_back();
         block->name = "Starters & boots";
         add_items_to_block(*block, data_role.at(0));
         add_items_to_block(*block, data_role.at(2));
-        item_candidates.clear();
 
         block = &item_set.blocks.emplace_back();
         block->name = "First items";
@@ -150,11 +159,13 @@ std::vector<ItemSet> Ugg::itemsets(Champion const& champion) const
         add_items_to_block(*block, data_role.at(5), false);
         add_items_to_block(*block, data_role.at(6));
 
-        item_candidates.clear();
+        item_set.blocks.push_back(miscellaneous_block);
 
-        for (ItemSet::Block const& block : item_set.blocks) {
-            fmt::print("{}: ", block.name);
-            for (ItemSet::Item const& item : block.items)
+
+        fmt::print("\n{}:\n", role.name);
+        for (ItemSet::Block const& bblock : item_set.blocks) {
+            fmt::print("\t{}: ", bblock.name);
+            for (ItemSet::Item const& item : bblock.items)
                 fmt::print("{}*{} ", item.id, item.count);
             fmt::print("\n");
         }
