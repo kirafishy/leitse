422cba33484ab03c4d77320c0df0827be5a11fe9
kiwixz <kiwixz@outlook.com>
2019-03-02 19:21:48 +0000

    fix missing items, cleaner output

--- leitse/ugg.cpp
+++ leitse/ugg.cpp
@@ -39,7 +39,7 @@ struct ItemCandidateComparator {
 
     bool operator()(int lhs, ItemCandidate const& rhs) const
     {
-        return !(*this)(rhs, lhs);
+        return lhs > rhs.id;
     }
 };
 
@@ -81,7 +81,7 @@ std::vector<ItemSet> Ugg::itemsets(Champion const& champion) const
     std::set<int> added_items;
     std::set<ItemCandidate, ItemCandidateComparator> item_candidates;
 
-    auto add_candidate = [&](ItemCandidate&& candidate) {
+    auto add_candidate = [&](ItemCandidate candidate) {
         if (added_items.count(candidate.id) != 0)
             return;
         auto it = item_candidates.find(candidate.id);  // we are comparing only ids
@@ -100,8 +100,10 @@ std::vector<ItemSet> Ugg::itemsets(Champion const& champion) const
         nlohmann::json const& data_role = data.at(role.id);
         ItemSet& item_set = item_sets.emplace_back(name(), ItemSet::Map::any, role.name);
 
-        for (nlohmann::json const& data_block : data_role) {
-            ItemSet::Block& block = item_set.blocks.emplace_back();
+        auto add_items_to_block = [&](ItemSet::Block& block,
+                                      nlohmann::json const& data_block,
+                                      bool add_candidates = true) {
+            int total_matches = 0;
 
             for (nlohmann::json const& data_candidate : data_block) {
                 nlohmann::json const& data_candidate_items = data_candidate.at(0);
@@ -110,32 +112,45 @@ std::vector<ItemSet> Ugg::itemsets(Champion const& champion) const
                     int matches = data_candidate.at(2).get<int>();
                     for (nlohmann::json const& data_candidate_item : data_candidate.at(0))
                         add_candidate({data_candidate_item.get<int>(), wins, matches});
+                    total_matches += matches;
                 }
                 else {
-                    int item_id = data_candidate_items.get<int>();
-                    if (added_items.count(item_id) == 0)
-                        add_candidate({item_id,
-                                       data_candidate.at(1).get<int>(),
-                                       data_candidate.at(2).get<int>()});
+                    int matches = data_candidate.at(2).get<int>();
+                    add_candidate({data_candidate_items.get<int>(),
+                                   data_candidate.at(1).get<int>(),
+                                   matches});
+                    total_matches += matches;
                 }
             }
 
-            for (int i = 0; i < 10 && !item_candidates.empty(); ++i) {
-                int item_id = item_candidates.extract(item_candidates.begin()).value().id;
-                block.items.push_back({item_id});
-                added_items.insert(item_id);
+            while (!item_candidates.empty()) {
+                ItemCandidate item = item_candidates.extract(item_candidates.begin()).value();
+                block.items.push_back({item.id, std::min(100 * item.matches / total_matches, 1)});
+                added_items.insert(item.id);
             }
-        }
+        };
+
+        ItemSet::Block* block = &item_set.blocks.emplace_back();
+        block->name = "Starters & boots";
+        add_items_to_block(*block, data_role.at(0));
+        add_items_to_block(*block, data_role.at(2));
+        item_candidates.clear();
+
+        block = &item_set.blocks.emplace_back();
+        block->name = "First items";
+        add_items_to_block(*block, data_role.at(1));
+
+        block = &item_set.blocks.emplace_back();
+        block->name = "Second items";
+        add_items_to_block(*block, data_role.at(3));
+
+        block = &item_set.blocks.emplace_back();
+        block->name = "Other items";
+        add_items_to_block(*block, data_role.at(4), false);
+        add_items_to_block(*block, data_role.at(5), false);
+        add_items_to_block(*block, data_role.at(6));
 
         item_candidates.clear();
-        std::swap(item_set.blocks[1], item_set.blocks[2]);
-        item_set.blocks[0].name = "Starting items";
-        item_set.blocks[1].name = "Boots";
-        item_set.blocks[2].name = "First items";
-        item_set.blocks[3].name = "Second items";
-        item_set.blocks[4].name = "Third items";
-        item_set.blocks[5].name = "Fourth items";
-        item_set.blocks[6].name = "Fifth items";
 
         for (ItemSet::Block const& block : item_set.blocks) {
             fmt::print("{}: ", block.name);
