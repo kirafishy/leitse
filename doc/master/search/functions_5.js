487f6d4e69be97c84a59c96c7029d4c162eb42b7
ci <ci>
2019-03-02 04:00:23 +0000

    [ci] update 'doc/master' commit 0e6617c2ea4123299253c5ddff98ec99e91bfe04

--- doc/master/search/functions_5.js
+++ doc/master/search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['fetch_5fdata',['fetch_data',['../structleitse_1_1_data_dragon.html#adf547f08bc967eaaf39de71aa5695edc',1,'leitse::DataDragon']]]
+  ['fetch_5fdata',['fetch_data',['../structleitse_1_1_data_dragon.html#ab42170e26e20cd3d715f3f3220019e7c',1,'leitse::DataDragon']]]
 ];
