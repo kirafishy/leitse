d9c20f00829ba2aa040ce3fc0718e74559af8a0f
ci <ci>
2019-03-02 03:47:36 +0000

    [ci] update 'doc/master' commit 33b034f1724d6506e9e93c61e67bad76712638e2

--- doc/master/search/all_c.js
+++ doc/master/search/all_c.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['name',['name',['../structleitse_1_1_champion.html#af62130a251291c083eeabd7998ad9140',1,'leitse::Champion::name()'],['../structleitse_1_1_item_set_1_1_block.html#a183828ea77cee99ede98eb24babe7edf',1,'leitse::ItemSet::Block::name()'],['../structleitse_1_1_aggregator.html#ac256f24a2717cf3f01605f0cad6120c8',1,'leitse::Aggregator::name()'],['../structleitse_1_1aggregators_1_1_ugg.html#a4f0878bc4ffe903ec60d84b2480ca596',1,'leitse::aggregators::Ugg::name()']]]
+  ['name',['name',['../structleitse_1_1_champion.html#af62130a251291c083eeabd7998ad9140',1,'leitse::Champion::name()'],['../structleitse_1_1_item_set_1_1_block.html#a183828ea77cee99ede98eb24babe7edf',1,'leitse::ItemSet::Block::name()'],['../structleitse_1_1_aggregator.html#a395f2723aec6fa2a495481a5af7cab28',1,'leitse::Aggregator::name()'],['../structleitse_1_1aggregators_1_1_ugg.html#ab6c33748b46ffff18e21894ae00036e1',1,'leitse::aggregators::Ugg::name()']]]
 ];
