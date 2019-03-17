86f2500497c2f957bf50022acd72f8cc17c07249
kiwixz <kiwixz@outlook.com>
2019-03-17 14:36:34 +0100

    create missing item set directories

--- leitse/main.cpp
+++ leitse/main.cpp
@@ -38,6 +38,7 @@ void main_impl(int argc, char** argv)
     utils::ThreadPool thread_pool{conf.get<size_t>("threads")};
     for (Champion const& champion : data_dragon.champions()) {
         std::filesystem::path item_set_dir = champions_dir / champion.id / "Recommended";
+        std::filesystem::create_directories(item_set_dir);
         for (std::unique_ptr<Aggregator> const& aggregator : aggregators)
             thread_pool.submit([&aggregator, &champion, item_set_dir] {
                 try {
